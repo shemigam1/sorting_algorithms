9f68f8dbbe01ccf02af49a1e23d294a5b218ef3a
semilore <semilore@omotade-michaels.com>
2023-10-24 20:34:37 +0100

    add: selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort algorithm
+ * @array: array
+ * @size: size
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			int temp = array[i];
+
+			array[i] = array[min];
+			array[min] = temp;
+		}
+		print_array(array, size);
+	}
+}
